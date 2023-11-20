088e965d9950d1a7f6e1956632eae57cd362188d
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-11-19 21:30:13 -0400

    CalculadoraAmorcito

---
+++ index.js
@@ -0,0 +1,21 @@
+//ELEMENTS HTML
+let buttons = document.getElementsByClassName('button');
+let borrar = document.querySelector('.del');
+let screen = document.querySelector('#screen');
+let result = document.querySelector('#result');
+let equal = document.querySelector('.equal');
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function (e) {
+        screen.textContent = screen.textContent + buttons[i].value;
+    };
+};
+
+borrar.onclick = function (e) {
+    result.textContent = 'Ingresa los puntos';
+    screen.textContent = '';
+};
+
+equal.onclick = function (e) {
+    result.textContent = 'Total= ' + Math.round(screen.innerHTML / 769) + ' $';
+};
